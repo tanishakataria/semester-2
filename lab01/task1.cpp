86a9984d06c7e9277e93cf030c8d441172feed92
tanishakataria <142868660+tanishakataria@users.noreply.github.com>
2024-01-31 23:11:56 +0500

    Create task1.cpp

---
+++ lab01/task1.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"enter the number";
+    cin>>n;
+    if(n%2==0){
+        cout<<" not prime number";
+    }
+        else{
+            cout<<"prime number";
+        }
+}
